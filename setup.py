8af5d958bc25aeb61ce96c6600bfe64c14f6c220
Matteo Cerutti <matteo.cerutti@rkrepsilon.com>
2016-09-16 13:08:55 +0100

    renaming project.

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 version = '0.0.1'
 
 setup(
-  name = 'cook-api',
+  name = 'cook-jobclient',
   packages = ['cook'],
   version = version,
-  description = 'Python library for Cook Mesos Framework API',
+  description = 'Python library for Cook Scheduler REST API',
   author = 'Matteo Cerutti',
   author_email = 'matteo.cerutti@hotmail.co.uk',
-  url = 'https://github.com/m4ce/cook-api-python',
-  download_url = 'https://github.com/m4ce/cook-api-python/tarball/%s' % (version,),
-  keywords = ['cook', 'mesos'],
+  url = 'https://github.com/m4ce/cook-jobclient-python',
+  download_url = 'https://github.com/m4ce/cook-jobclient-python/tarball/%s' % (version,),
+  keywords = ['cook', 'mesos', 'jobclient'],
   classifiers = [],
   install_requires = ["requests", "requests-kerberos"]
 )
